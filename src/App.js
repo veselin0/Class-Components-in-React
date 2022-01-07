0bbca0a1c7d2126b7851e3f37f2fe2d3b1160016
Veselin <vesselin60@gmail.com>
2022-01-07 11:11:47 +0200

    Refactor function into class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App({type}) {
+//     return  (
+//                     <div>
+//                         <h1>{type} component</h1>
+//                     </div>
+//                 );
+// }
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.type} component</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
